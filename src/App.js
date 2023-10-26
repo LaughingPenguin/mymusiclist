2419ff8bfac7b529d58f67704593e9956ef42f0a
Haoran Xu <sxu2469@gmail.com>
2023-10-26 18:34:03 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import NavBar from "./components/navbar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
 
 export default App;
+library.add(fas)
