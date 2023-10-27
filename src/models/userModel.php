35203b52b6923f2fb8bcfa6b9727cc43b4045a6f
Haoran Xu <sxu2469@gmail.com>
2023-10-27 15:54:58 -0400

    implemented add user

--- src/models/userModel.php
+++ src/models/userModel.php
@@ -2,7 +2,24 @@
 require_once PROJECT_ROOT_PATH . "/models/database.php";
 
 class UserModel extends Database {
-    public function createUser () {
+    public function createUser ($email, $password) {
+        $checkUserQuery = $this->connection->prepare("SELECT id FROM users WHERE email = ?");
+        $checkUserQuery->bind_param("s", $email);
+        $checkUserQuery->execute();
+        $checkUserQuery->store_result();
+        if ($checkUserQuery->num_rows == 0) {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $createUserQuery = $this->connection->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+            $createUserQuery->bind_param("ss", $email, $hashedPassword);
+            if ($createUserQuery->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            echo "User already exists.";
+        }
+        $this->closeConnection();
     }
 
     public function updateUser () {
@@ -11,10 +28,10 @@ class UserModel extends Database {
     public function deleteUser () {
     }
 
-    public function deleteUserById () {
+    public function getUsers () {
     }
 
-    public function getUsers () {
+    public function getUserById () {
     }
 }
 
