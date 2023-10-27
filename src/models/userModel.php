083233e6d80ae54f37ab43b2e55d71b373e57e98
Haoran Xu <sxu2469@gmail.com>
2023-10-27 12:52:50 -0400

    created schema for userModel

---
+++ src/models/userModel.php
@@ -0,0 +1,21 @@
+<?php
+require_once PROJECT_ROOT_PATH . "/models/database.php";
+
+class UserModel extends Database {
+    public function createUser () {
+    }
+
+    public function updateUser () {
+    }
+
+    public function deleteUser () {
+    }
+
+    public function deleteUserById () {
+    }
+
+    public function getUsers () {
+    }
+}
+
+?>
