8d72dada64c4469c1625a29b77354dc5f4473a5d
Haoran Xu <sxu2469@gmail.com>
2023-10-28 16:47:25 -0400

    stream lined syntax between view and controller

--- src/controllers/userController.php
+++ src/controllers/userController.php
@@ -1,7 +1,7 @@
 <?php
 
 class UserController extends BaseController {
-    public function createAction () {
+    public function signupAction () {
         $requestMethod = $_SERVER["REQUEST_METHOD"];
         if (strtoupper ($requestMethod) == "POST") {
             $postData = json_decode(file_get_contents("php://input"), true);
