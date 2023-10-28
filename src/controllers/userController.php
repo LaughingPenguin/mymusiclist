9e5168b524e5ca6ffd84278e26c2c1e05a485d2b
Haoran Xu <sxu2469@gmail.com>
2023-10-27 23:53:44 -0400

    implemented update password

--- src/controllers/userController.php
+++ src/controllers/userController.php
@@ -14,6 +14,29 @@ class UserController extends BaseController {
             }
         }
     }
+
+    /* editAction enables the user to change their password by,
+     * checking for an UPDATE API request from the client server,
+     * extracting the JSON file for newpassword and oldpassword,
+     * verifying that the old password provided by the user is correct,
+     * and updating the user's password with the new password.
+     */
+    public function editAction () {
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        if (strtoupper ($requestMethod) == "UPDATE") {
+            $postData = json_decode(file_get_contents("php://input"), true);
+            $userModel = new UserModel();
+            if ($userModel->verifyUserPassword($_SESSION["email"], $postData["oldpassword"])) {
+                if ($userModel->updateUserPassword($_SESSION["email"], $postData["newpassword"])) {
+                    echo "Your password has been successfully updated";
+                } else {
+                    echo "There's was an error. Please try again.";
+                }
+            } else {
+                echo "The password you entered is incorrect.";
+            }
+        }
+    }
 }
 
 ?>
