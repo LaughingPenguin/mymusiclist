b26180ae0a5481f7b74a5ffa0c59673c16d0e55e
Haoran Xu <sxu2469@gmail.com>
2023-11-02 03:51:47 -0400

    rewrote as functional component

--- src/views/signupView.js
+++ src/views/signupView.js
@@ -1,57 +1,72 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import './signup.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './signup.css';
 
-class SignUp extends Component {
-    state = {
-        data : {
-            username : "",
-            email : "",
-            password : "",
-        }
-    }
-    handleChange = (e) => {
-        const { name, value } = e.target
-        this.setState((prevState) => ({
-            data : {
-                ...prevState.data,
-                [name] : value,
-            }
-        }))
-    }
-    handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post("http://localhost:8080/index.php/user/signup", this.state.data)
+function SignUp() {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name] : value,
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8080/index.php/user/signup', formData)
             .then((response) => {
-                console.log('Form data submitted successfully', response)
+                if (response.status === 201) {
+                    console.log("Account created", response);
+                    navigate("/login");
+                }
             })
             .catch((error) => {
-                console.error('Error submitting form data', error)
-            })
-    }
-    render() {
-        return (
-            <body class="text-center d-flex flex-column justify-content-center bg-Platinum">
-              <form class="form-signup" onSubmit={this.handleSubmit}>
-                <h1 class="h1 mb-5">mymusiclist</h1>
-                <h1 class="h3 mb-3">Register for an account</h1>
-                <input
-                    type="username" name="username"
-                    class="form-control" placeholder="Username"
-                    value={this.state.data.username} onChange={this.handleChange} />
-                <input
-                    type="email" name="email"
-                    class="form-control" placeholder="Email"
-                    value={this.state.data.email} onChange={this.handleChange} />
-                <input
-                    type="password" name="password"
-                    class="form-control" placeholder="Password"
-                    value={this.state.data.password} onChange={this.handleChange} />
-                <button class="btn btn-lg btn-primary border-0 mt-1" type="submit">Sign Up</button>
-              </form>
-            </body>
-        )
-    }
+                if (error.status === 409) {
+                    console.error("Account already exists", error);
+                }
+            });
+    };
+    return (
+        <div className="text-center d-flex flex-column justify-content-center vh-100 bg-Platinum">
+            <form className="form-signup" onSubmit={handleSubmit}>
+            <h1 className="h1 mb-5">mymusiclist</h1>
+            <h1 className="h3 mb-3">Register for an account</h1>
+            <input
+                type="username"
+                name="username"
+                className="form-control"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+            <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <input
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <button className="btn btn-lg btn-primary border-0 mt-1" type="submit">
+                Sign Up
+            </button>
+            </form>
+        </div>
+    );
 }
 
-export default SignUp
+export default SignUp;
