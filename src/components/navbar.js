bb984d7ff6c23e6822bd0e6a1ba3bdada93643fc
Haoran Xu <sxu2469@gmail.com>
2023-11-03 03:58:48 -0400

    added dynamic navbar and logout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,49 +1,103 @@
 import React from "react"
-import { Link } from 'react-router-dom'
+import { useReducer } from "react"
+import { useNavigate, Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-YlnMnBlue navbar-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand">mymusiclist</a>
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+    function handleClick() {
+        forceUpdate();
+    }
 
-                <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#hamburger" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="hamburger">
-                    <ul className="navbar-nav d-flex text-center me-auto">
-                        <li className="nav-item me-3 me-lg-0">
-                            <a className="nav-link" href="/#home-section"> Home </a>
-                        </li>
-                        <li className="nav-item me-3 me-lg-0">
-                            <a className="nav-link" href="/#charts-section"> Charts </a>
-                        </li>
-                        <li className="nav-item me-3 me-lg-0">
-                            <a className="nav-link" href="/#faqs-section"> FAQs </a>
-                        </li>
-                        <li className="nav-item me-3 me-lg-0">
-                            <a className="nav-link" href="/#contact-section"> Contact </a>
-                        </li>
-                        <li className="nav-item me-3 me-lg-0">
-                            <LinkContainer to="/login"><a className="nav-link"> Login/SignUp</a></LinkContainer>
-                        </li>
-                    </ul>
-                    <form className="d-flex input-group input-group-sm w-auto border-0">
-                        <input
-                        type="search"
-                        className="form-control"
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="search-addon"
-                        />
-                        <button className="btn btn-outline-light bg-YlnMnLightBlue" type="submit" id="button-addon2"><FontAwesomeIcon icon="fas fa-search" /></button>
-                    </form>
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.removeItem("token");
+        navigate('/', { replace: true });
+    }
+    if (localStorage.getItem("token") != null) {
+        return (
+            <nav className="navbar navbar-expand-lg bg-YlnMnBlue navbar-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand">mymusiclist</a>
+
+                    <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#hamburger" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="hamburger">
+                        <ul className="navbar-nav d-flex text-center me-auto">
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#home-section"> Home </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#charts-section"> Charts </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#faqs-section"> FAQs </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#contact-section"> Contact </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" type="button" onClick={logout}> Log Out </a>
+                            </li>
+                        </ul>
+                        <form className="d-flex input-group input-group-sm w-auto border-0">
+                            <input
+                            type="search"
+                            className="form-control"
+                            placeholder="Search"
+                            aria-label="Search"
+                            aria-describedby="search-addon"
+                            />
+                            <button className="btn btn-outline-light bg-YlnMnLightBlue" type="submit" id="button-addon2"><FontAwesomeIcon icon="fas fa-search" /></button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    } else {
+        return (
+            <nav className="navbar navbar-expand-lg bg-YlnMnBlue navbar-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand">mymusiclist</a>
+
+                    <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#hamburger" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="hamburger">
+                        <ul className="navbar-nav d-flex text-center me-auto">
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#home-section"> Home </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#charts-section"> Charts </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#faqs-section"> FAQs </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <a className="nav-link" href="/#contact-section"> Contact </a>
+                            </li>
+                            <li className="nav-item me-3 me-lg-0">
+                                <LinkContainer to="/login"><a className="nav-link" type="button"> Login/SignUp </a></LinkContainer>
+                            </li>
+                        </ul>
+                        <form className="d-flex input-group input-group-sm w-auto border-0">
+                            <input
+                            type="search"
+                            className="form-control"
+                            placeholder="Search"
+                            aria-label="Search"
+                            aria-describedby="search-addon"
+                            />
+                            <button className="btn btn-outline-light bg-YlnMnLightBlue" type="submit" id="button-addon2"><FontAwesomeIcon icon="fas fa-search" /></button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+
 }
 export default NavBar
