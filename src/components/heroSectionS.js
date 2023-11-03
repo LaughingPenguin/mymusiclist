d0c2e101d2cddde9606cea2af6d29336e82c3193
Haoran Xu <sxu2469@gmail.com>
2023-11-03 00:49:30 -0400

    improved responsiveness of navbar and homepage

--- src/components/heroSectionS.js
+++ src/components/heroSectionS.js
@@ -3,13 +3,13 @@ import { rateimg, reviewimg, searchimg } from '.';
 
 const HeroTwo = () => {
     return (
-        <div class="container-lg pt-5 my-5 text-center">
+        <div class="container pt-5 my-5 text-center">
             <h1 class="display-5 fw-bold lh-1">Simple and easy to use</h1>
             <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4">with a free online account.</p>
             </div>
             <div class="row row-cols-1 row-cols-md-3 g-5">
-                <div class="col">
+                <div class="col-sm">
                     <div class="card">
                         <img src={searchimg} class="card-img-top bg-YlnMnLightBlue" alt="" />
                         <div class="card-body">
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,48 +1,47 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-YlnMnBlue navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">mymusiclist</a>
+        <nav className="navbar navbar-expand-lg bg-YlnMnBlue navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand">mymusiclist</a>
 
-                <form class="d-flex input-group input-group-sm w-auto border-0">
-                    <input
-                    type="search"
-                    class="form-control"
-                    placeholder="Search"
-                    aria-label="Search"
-                    aria-describedby="search-addon"
-                    />
-                    <button class="btn btn-outline-light bg-YlnMnLightBlue" type="submit" id="button-addon2"><FontAwesomeIcon icon="fas fa-search" /></button>
-                </form>
-
-                <ul class="navbar-nav d-flex flex-row me-1">
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="/#home-section"> Home </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="/#charts-section"> Charts </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="/#about-section"> About </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="/#faqs-section"> FAQs </a>
-                    </li>
-                    <li class="nav-item me-3 me-lg-0">
-                        <a class="nav-link" href="/#contact-section"> Contact </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><FontAwesomeIcon icon="fas fa-user" /></a>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li class="dropdown-item"><Link to="/login"> My Account </Link></li>
-                            <li class="dropdown-item"><Link to="/"> Log Out </Link></li>
-                        </ul>
-                    </li>
-                </ul>
+                <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#hamburger" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="hamburger">
+                    <ul className="navbar-nav d-flex text-center me-auto">
+                        <li className="nav-item me-3 me-lg-0">
+                            <a className="nav-link" href="/#home-section"> Home </a>
+                        </li>
+                        <li className="nav-item me-3 me-lg-0">
+                            <a className="nav-link" href="/#charts-section"> Charts </a>
+                        </li>
+                        <li className="nav-item me-3 me-lg-0">
+                            <a className="nav-link" href="/#faqs-section"> FAQs </a>
+                        </li>
+                        <li className="nav-item me-3 me-lg-0">
+                            <a className="nav-link" href="/#contact-section"> Contact </a>
+                        </li>
+                        <li className="nav-item me-3 me-lg-0">
+                            <LinkContainer to="/login"><a className="nav-link"> Login/SignUp</a></LinkContainer>
+                        </li>
+                    </ul>
+                    <form className="d-flex input-group input-group-sm w-auto border-0">
+                        <input
+                        type="search"
+                        className="form-control"
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="search-addon"
+                        />
+                        <button className="btn btn-outline-light bg-YlnMnLightBlue" type="submit" id="button-addon2"><FontAwesomeIcon icon="fas fa-search" /></button>
+                    </form>
+                </div>
             </div>
         </nav>
     )
